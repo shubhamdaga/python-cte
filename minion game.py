8841be41daa89ff7ac2cf75a500b997b9f318645
f20180134 <f20180134@goa.bits-pilani.ac.in>
2018-11-12 18:17:14 +0530

    from linux

--- minion game.py
+++ minion game.py
@@ -1,36 +1,18 @@
-def minion_game(string):
+def merge_the_tools(string, k):
     # your code goes here
-    stuart=0
-    kevin=0
-    Kevin=''
-    Stuart=''
-    for i in range(len(string)):
-        if string[i] in 'AEIOU':
-           for j in range(i,len(string)):
-            a=''
-            x=1
-            for k in range(x):
-                a=string[i,(i+k+1)]
-            x=x+1
-            kevin.append(a)
-        else:
-            for j in range(i,len(string)):
-                a=''
-                x=1
-                for k in range(x):
-                    a=string[i,(i+k+1)]
-                x=x+1
-                stuart.append(a)
-    Kevin=list(set(Kevin))
-    Stuart=list(set(Stuart))
-    kevin=len(Kevin)
-    stuart=len(Stuart)
-    if kevin > stuart:
-        print('Kevin {}'.format(kevin))
-    elif Stuart>kevin:
-        print('Stuart {}'.format(Stuart))
-    else:
-        print('Draw')
+    n=(len(string))
+    y=int(n/k)
+    for i in range(0,n,y):
+        a=string[i:(i+y)]
+        f=''
+        for j in range(len(a)):
+            e=a[j]
+            if a[j] in f:
+                e=''
+            f=f+e
+        print(f)
+    print(y)
+    print(n)
 if __name__ == '__main__':
-    s = input()
-    minion_game(s)
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
